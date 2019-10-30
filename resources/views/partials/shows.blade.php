305898e10da8598f78ef3d9c9dece47702b69aab
Daniel Lucia <daniellucia84@gmail.com>
2019-10-30 16:54:41 +0100

    Corrección home

--- resources/views/partials/shows.blade.php
+++ resources/views/partials/shows.blade.php
@@ -1,6 +1,7 @@
 <div class="shows">
-    <h3>{{$category->name}}</h3>
+
     @if (!empty($shows))
+        <h3>{{$category->name}}</h3>
         @foreach ($shows as $show)
             <p><a href="{{ route('show.view', [$category, $show]) }}">{{ $show->name }}</a></p>
         @endforeach
