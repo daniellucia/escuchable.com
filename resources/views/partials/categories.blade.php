12b87f6025ba5afbf4709524bede8ea05d074c77
Daniel Lucia <daniellucia84@gmail.com>
2019-11-01 01:06:47 +0100

    Correcciones varias

--- resources/views/partials/categories.blade.php
+++ resources/views/partials/categories.blade.php
@@ -3,7 +3,9 @@
         <p class="Title Sticky">Categorías</p>
         <ul>
         @foreach ($categories as $categoryItem)
-            <li><a href="{{ route('category.view', $categoryItem) }}">{{ $categoryItem->name }}</a></li>
+            <li id="{{ $categoryItem->slug }}" class=" @if(isset($category) && $categoryItem == $category) Selected @endif">
+                <a href="{{ route('category.view', $categoryItem) }}">{{ $categoryItem->name }}</a>
+            </li>
         @endforeach
         </ul>
     </div>
--- resources/views/partials/shows.blade.php
+++ resources/views/partials/shows.blade.php
@@ -6,13 +6,15 @@
         {{ $shows->links() }}
         <ul>
         @foreach ($shows as $showItem)
-            <li class="ShowSummary">
+            <li class="ShowSummary @if(isset($show) && $showItem == $show) Selected @endif" id="{{ $showItem->slug }}">
                 @if ($showItem->thumbnail != '')
                 <p class="Image"><img width="40" height="40" src="{{ asset($showItem->thumbnail) }}" alt="{{ $showItem->name }}" /></p>
                 @endif
 
-                <h5><a href="{{ route('show.view', [$category, $showItem, 'page-show' => app('request')->input('page-show')]) }}">{{ $showItem->name }}</a></h5>
-                <p class="Description">{{ Str::limit(strip_tags($showItem->description), 110) }}</p>
+                <h5><a href="{{ route('show.view', [$category, $showItem, 'page-show' => app('request')->input('page-show'), '#'.$showItem->slug]) }}">{{ $showItem->name }}</a></h5>
+                <p class="Description">
+                {{ Str::limit(strip_tags($showItem->description), 110) }}
+                </p>
             </li>
         @endforeach
         </ul>
