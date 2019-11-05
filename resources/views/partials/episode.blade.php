05865f169e33c7fda414221d4068ab209708ed1d
Daniel Lucia <daniellucia84@gmail.com>
2019-11-05 22:43:53 +0100

    Añadida fecha del episodio

--- resources/views/partials/episode.blade.php
+++ resources/views/partials/episode.blade.php
@@ -2,6 +2,9 @@
     <p class="Back"><a href="{{ route('show.view', [$show])}}">Volver</a></p>
     <div class="ShowDescription Sticky">
         <h1>{{$episode->title}}</h1>
+        <p>
+        {{ \Carbon\Carbon::createFromTimeStamp(strtotime($episode->published))->diffForHumans() }}
+        </p>
         <p>{!! $episode->description !!}</p>
     </div>
 </div>
