48388ffe7e7a14ac0c98cd88899cb776e6ec0564
Daniel Lucia <daniellucia84@gmail.com>
2019-11-05 20:17:22 +0100

    Quitado hash del menú de categorias

--- resources/views/partials/categories.blade.php
+++ resources/views/partials/categories.blade.php
@@ -4,7 +4,7 @@
         <ul @if(isset($category)) class="categorySelected" @endif >
         @foreach ($categories as $categoryItem)
             <li id="{{ $categoryItem->slug }}" class=" @if(isset($category) && $categoryItem == $category) Selected @endif">
-                <a href="{{ route('category.view', [$categoryItem, '#'.$categoryItem->slug]) }}">{{ ucfirst($categoryItem->name) }}</a>
+                <a href="{{ route('category.view', $categoryItem) }}">{{ ucfirst($categoryItem->name) }}</a>
             </li>
         @endforeach
         </ul>
--- resources/views/partials/episodes.blade.php
+++ resources/views/partials/episodes.blade.php
@@ -1,6 +1,6 @@
 <div class="Episodes">
     @if (!empty($episodes))
-        <p class="Back"><a href="{{ route('category.view', [$category])}}">Volver</a></p>
+        <p class="Back"><a href="{{ route('category.view', $category)}}">Volver</a></p>
         <div class="ShowDescription Sticky">
             <h1>{{$show->name}}</h1>
             <p>{!! $show->description !!}</p>
