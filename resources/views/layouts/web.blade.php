206c4a73219daa8496824417f3cc55051d38f6b4
Daniel Lucia <daniellucia84@gmail.com>
2019-11-06 11:31:49 +0100

    Añadido favicon y restructura metatags

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -1,10 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    {!! MetaTag::render() !!}
+    @include('partials.metatags')
     <link href="{{ asset('css/fonts/apercu.css') }}" rel="stylesheet">
     <link href="{{ asset('css/fonts/source-sans.css') }}" rel="stylesheet">
     <link href="{{ asset('css/web.css') }}?v=20191106" rel="stylesheet">
