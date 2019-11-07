6e956cce0af41fec2cf9a32b60cf0419431bc214
Daniel Lucia <daniellucia84@gmail.com>
2019-11-07 12:51:48 +0100

    Modificado reproducto.Se usa el nativo.

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -4,7 +4,7 @@
     @include('partials.metatags')
     <link href="{{ asset('css/fonts/apercu.css') }}" rel="stylesheet">
     <link href="{{ asset('css/fonts/source-sans.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/web.css') }}?v=20191107a" rel="stylesheet">
+    <link href="{{ asset('css/web.css') }}?v=20191107b" rel="stylesheet">
 </head>
 <body>
 
