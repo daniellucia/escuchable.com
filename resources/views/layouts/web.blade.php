49f48518423163093bb6d48fe2564b851dcd24d7
Daniel Lucia <daniellucia84@gmail.com>
2019-11-07 13:15:31 +0100

    Corregido margen del botón atrás

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -4,7 +4,7 @@
     @include('partials.metatags')
     <link href="{{ asset('css/fonts/apercu.css') }}" rel="stylesheet">
     <link href="{{ asset('css/fonts/source-sans.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/web.css') }}?v=20191107b" rel="stylesheet">
+    <link href="{{ asset('css/web.css') }}?v=20191107c" rel="stylesheet">
 </head>
 <body>
 
