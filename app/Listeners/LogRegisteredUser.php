9e826ff32956d2680ddfd33b6024b0409d40dd37
Daniel Lucia <daniellucia84@gmail.com>
2019-11-08 20:59:13 +0100

    Le asignamos el rol user a todos los usuarios nuevos que se reistern

---
+++ app/Listeners/LogRegisteredUser.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class LogRegisteredUser
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Registered  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        /**
+         * Damos permiso de usuario.
+         * En el caso de hacer un uso anomalo
+         * se le retira el rol.
+         */
+        $event->user->assignRole('user');
+    }
+}
