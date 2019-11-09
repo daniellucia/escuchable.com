30b5604260fb8302f3648af8a99325a6c134b155
Daniel Lucia <daniellucia84@gmail.com>
2019-11-09 23:41:06 +0100

    Cambio para que comprueba tambien el titulo

--- app/Resources/Feed.php
+++ app/Resources/Feed.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Resources;
+
+use App\Resources\Channel;
 use App\Resources\Read;
 use App\Shows;
-use App\Resources\Channel;
 use Illuminate\Support\Facades\Auth;
 
 class Feed
@@ -12,25 +13,28 @@ class Feed
     public static function insert(string $feed)
     {
         $feed = trim($feed);
-            $show = Shows::where('feed', $feed)->first();
+        $show = Shows::where('feed', $feed)->first();
 
-            if (!$show) {
-                $xml = null;
-                try {
-                    $xml = Read::xml($feed);
-                } catch (Exception $e) {
-                    return false;
-                }
+        if (!$show) {
+            $xml = null;
+            try {
+                $xml = Read::xml($feed);
+            } catch (Exception $e) {
+                return false;
+            }
 
-                if (is_object($xml)) {
-                    $channel = new Channel($xml->channel);
-                    $data = $channel->toArray();
+            if (is_object($xml)) {
+                $channel = new Channel($xml->channel);
+                $data = $channel->toArray();
+
+                $show = Shows::where('name', $data['name'])->first();
+
+                if (!$show) {
                     $data['categories_id'] = intval($data['categories_id']);
 
                     $data['feed'] = $feed;
 
-                    if($user = Auth::user())
-                    {
+                    if ($user = Auth::user()) {
                         $data['user_id'] = $user->id;
                     }
                     $show = Shows::create($data);
@@ -41,7 +45,9 @@ class Feed
 
             }
 
-            return false;
+        }
+
+        return false;
     }
 
 }
