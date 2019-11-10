7b5e2c19001d2a7c7b25403ee90088fa5849d4a8
Daniel Lucia <daniellucia84@gmail.com>
2019-11-10 21:38:50 +0100

    No mostramos el paginador cuando no hay episodios

--- resources/views/partials/pagination.blade.php
+++ resources/views/partials/pagination.blade.php
@@ -1,4 +1,6 @@
-<div class="PaginationContent">
-    <p><strong>{{ $element->total() }}</strong> <em>{{$text}}</em></p>
-    {{ $element->links() }}
-</div>
+@if ($element->total() > 0)
+    <div class="PaginationContent">
+        <p><strong>{{ $element->total() }}</strong> <em>{{$text}}</em></p>
+        {{ $element->links() }}
+    </div>
+@endif
